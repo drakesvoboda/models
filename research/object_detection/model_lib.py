e752f3f35d6c3c359ed6d7f46b23f6c42c0e5ad6
drakesvoboda <drake@thinkmoncur.com>
2018-07-25 17:33:42 -0400

    object_detection: range() -> list(range()) and .itervalues() -> .values() for python 3 compatilbility. Fixes #4832

--- research/object_detection/model_lib.py
+++ research/object_detection/model_lib.py
@@ -279,7 +279,7 @@ def create_model_fn(detection_model_fn, configs, hparams, use_tpu=False):
     if mode in (tf.estimator.ModeKeys.TRAIN, tf.estimator.ModeKeys.EVAL):
       losses_dict = detection_model.loss(
           prediction_dict, features[fields.InputDataFields.true_image_shape])
-      losses = [loss_tensor for loss_tensor in losses_dict.itervalues()]
+      losses = [loss_tensor for loss_tensor in losses_dict.values()]
       if train_config.add_regularization_loss:
         regularization_losses = tf.get_collection(
             tf.GraphKeys.REGULARIZATION_LOSSES)
@@ -378,7 +378,7 @@ def create_model_fn(detection_model_fn, configs, hparams, use_tpu=False):
         eval_metrics = ['coco_detection_metrics']
       eval_metric_ops = eval_util.get_eval_metric_ops_for_evaluators(
           eval_metrics,
-          category_index.values(),
+          list(category_index.values()),
           eval_dict,
           include_metrics_per_category=eval_config.include_metrics_per_category)
       for loss_key, loss_tensor in iter(losses_dict.items()):
@@ -388,7 +388,7 @@ def create_model_fn(detection_model_fn, configs, hparams, use_tpu=False):
       if img_summary is not None:
         eval_metric_ops['Detections_Left_Groundtruth_Right'] = (
             img_summary, tf.no_op())
-      eval_metric_ops = {str(k): v for k, v in eval_metric_ops.iteritems()}
+      eval_metric_ops = {str(k): v for k, v in eval_metric_ops.items()}
 
       if eval_config.use_moving_averages:
         variable_averages = tf.train.ExponentialMovingAverage(0.0)
